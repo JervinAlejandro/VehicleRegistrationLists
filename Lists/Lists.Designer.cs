e83f4c9db1c436f971f0a33c00a274b546bc29dc
JervinAlejandro <90294855+JervinAlejandro@users.noreply.github.com>
2021-11-08 10:14:23 +0800

    Add files via upload

---
+++ Lists/Lists.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace Lists
+{
+    partial class Lists
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listDisplay = new System.Windows.Forms.ListBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listDisplay
+            // 
+            this.listDisplay.FormattingEnabled = true;
+            this.listDisplay.Location = new System.Drawing.Point(12, 103);
+            this.listDisplay.Name = "listDisplay";
+            this.listDisplay.Size = new System.Drawing.Size(275, 251);
+            this.listDisplay.TabIndex = 0;
+            this.listDisplay.SelectedIndexChanged += new System.EventHandler(this.listDisplay_SelectedIndexChanged);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(12, 12);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(275, 20);
+            this.textBoxInput.TabIndex = 1;
+            this.textBoxInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxInput_KeyPress);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(12, 38);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(121, 52);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "ENTER";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(155, 38);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(132, 23);
+            this.deleteButton.TabIndex = 3;
+            this.deleteButton.Text = "DELETE";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(107, 364);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(87, 23);
+            this.searchButton.TabIndex = 4;
+            this.searchButton.Text = "BIN SEARCH";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(200, 402);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(87, 23);
+            this.openButton.TabIndex = 5;
+            this.openButton.Text = "OPEN";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(107, 402);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(87, 23);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "SAVE";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(155, 67);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(132, 23);
+            this.buttonEdit.TabIndex = 7;
+            this.buttonEdit.Text = "EDIT";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(200, 364);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "LIN SEARCH";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 364);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "TAG";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(12, 403);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(87, 23);
+            this.buttonReset.TabIndex = 11;
+            this.buttonReset.Text = "RESET";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // Lists
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 443);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.listDisplay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Lists";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "List of Colours";
+            this.Load += new System.EventHandler(this.Lists_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listDisplay;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
+
---
+++ Lists/Lists.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Lists
+{
+    public partial class Lists : Form
+    {
+        public Lists()
+        {
+            InitializeComponent();
+        }
+        List<string> RegoPlate = new List<string>();
+
+        #region FUNCTIONS
+        private void DisplayList()
+        {
+            listDisplay.Items.Clear();
+            //RegoPlate.Sort();
+            foreach (var rego in RegoPlate)
+            {
+                listDisplay.Items.Add(rego);
+            }
+        }
+        private void Lists_Load(object sender, EventArgs e)
+        {
+            string fileName = "demo_01.txt";
+
+            using (Stream openFile = File.Open(fileName, FileMode.Open))
+            {
+                BinaryFormatter binFormat = new BinaryFormatter();
+                while (openFile.Position < openFile.Length)
+                {
+                    RegoPlate.Add((string)binFormat.Deserialize(openFile));
+                }
+            }
+            DisplayList();
+
+        }
+        private void listDisplay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listDisplay.SelectedIndex > -1)
+            {
+                textBoxInput.Text = listDisplay.SelectedItem.ToString();
+            }      
+                
+        }
+        // Prevent user from pressing enter key
+        private void textBoxInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+            }
+        }
+
+        #endregion FUNCTIONS
+
+
+        #region BUTTONS
+        private void addButton_Click(object sender, EventArgs e)
+        {      
+            if (!String.IsNullOrEmpty(textBoxInput.Text) && !RegoPlate.Contains(textBoxInput.Text))
+            {
+                RegoPlate.Add(textBoxInput.Text);
+                textBoxInput.Clear();
+                textBoxInput.Select();
+                DisplayList();
+            }
+            else
+            {
+                MessageBox.Show("ADD Fail");
+            }
+           
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(textBoxInput.Text) && RegoPlate.Contains(textBoxInput.Text))
+            {
+                RegoPlate.Remove(textBoxInput.Text);
+                textBoxInput.Clear();
+                textBoxInput.Select();
+                DisplayList();
+            }
+            else
+            {
+                MessageBox.Show("DELETE Fail");
+            }
+            
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            if (listDisplay.SelectedIndex > -1 && !String.IsNullOrEmpty(textBoxInput.Text))
+            {
+                RegoPlate[listDisplay.SelectedIndex] = textBoxInput.Text;
+                textBoxInput.Clear();
+                textBoxInput.Select();
+                DisplayList();
+            }
+            else
+            {
+                MessageBox.Show("EDIT Fail");
+            }
+
+        }
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            RegoPlate.Sort();
+            DisplayList();
+            if (RegoPlate.BinarySearch(textBoxInput.Text) >= 0) // >= 0 is a comparator
+            {
+                listDisplay.SelectedItem = textBoxInput.Text;
+                
+                MessageBox.Show("Found");
+            }
+            else
+                MessageBox.Show("Not Found");
+            textBoxInput.Clear(); // Clear textbox once found
+        }
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            string fileName = "demo_nn.txt";
+            OpenFileDialog OpenText = new OpenFileDialog();
+            DialogResult sr = OpenText.ShowDialog();
+            if (sr == DialogResult.OK)
+            {
+                fileName = OpenText.FileName;
+                try
+                {
+                    RegoPlate.Clear();
+                    using (Stream stream = File.Open(fileName, FileMode.Open))
+                    {
+                        BinaryFormatter binaryFormatter = new BinaryFormatter();
+                        while (stream.Position < stream.Length)
+                        {
+                            RegoPlate.Add((string)binaryFormatter.Deserialize(stream));
+                        }
+                    }
+                    DisplayList();
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Cannot Open File");
+                }
+            }
+            
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string fileName = "demo_nn.txt";
+            SaveFileDialog SaveText = new SaveFileDialog();
+            DialogResult sr = SaveText.ShowDialog();
+            if (sr == DialogResult.Cancel)
+            {
+                SaveText.FileName = fileName;
+            }
+            if (sr == DialogResult.OK)
+            {
+                fileName = SaveText.FileName;
+            }
+            try
+            {
+                using (Stream stream = File.Open(fileName, FileMode.Create))
+                {
+                    BinaryFormatter binaryFormatter = new BinaryFormatter();
+                    foreach (var item in RegoPlate)
+                    {
+                        binaryFormatter.Serialize(stream, item);
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Cannot Save File");
+            }
+        }
+
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            RegoPlate.Clear();
+            textBoxInput.Clear();
+            DisplayList();
+        }
+
+        #endregion BUTTONS
+
+
+    }
+}
